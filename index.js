33ec0b54c7de8507da468ad9b8a3bec8f9d917c8
Alesh Houdek <mail@alesh.com>
2017-07-07 02:27:45 -0600

    imports twitter credentials from module

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var Twit = require('twit')
+var cred = require('./credentials.js')
 
 var T = new Twit({
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
+  consumer_key:         cred.consumer_key,
+  consumer_secret:      cred.consumer_secret,
+  access_token:         cred.access_token,
+  access_token_secret:  cred.access_token_secret,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
+
+console.log('cred.consumer_key: ', cred.consumer_key);
 
 //
 //  tweet 'hello world!'
 //
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data)
-})
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+// })
